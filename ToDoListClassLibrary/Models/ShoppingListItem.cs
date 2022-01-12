7a86398b9c07cec9b4f683415d3463dc8749c863
Minhly <minh.y.ly@hotmail.com>
2022-01-12 09:52:17 +0100

    ShoppingListAPI

---
+++ ToDoListClassLibrary/Models/ShoppingListItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace ToDoListClassLibrary.Models
+{
+    public partial class ShoppingListItem
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Required]
+        [Column("title")]
+        [StringLength(50)]
+        public string Title { get; set; }
+        [Required]
+        [Column("checked")]
+        [StringLength(10)]
+        public string Checked { get; set; }
+        [Column("shoppinglistId")]
+        public int ShoppinglistId { get; set; }
+
+        [ForeignKey(nameof(ShoppinglistId))]
+        [InverseProperty("ShoppingListItems")]
+        public virtual Shoppinglist Shoppinglist { get; set; }
+    }
+}
