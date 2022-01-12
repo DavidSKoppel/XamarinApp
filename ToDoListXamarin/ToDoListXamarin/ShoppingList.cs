4b4e32028aba9673f2e3aa527ff6b4f4b700a829
DavidSKoppel <davi3442@edu.sde.dk>
2022-01-12 12:19:54 +0100

    Added Frontpage

---
+++ ToDoListXamarin/ToDoListXamarin/ShoppingList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoListXamarin
+{
+    public class ShoppingList
+    {
+        public string Title { get; set; }
+        public string ShoppingDate { get; set; }
+        public string ShoppingItems { get; set; }
+
+        public ShoppingList(string title, string shoppingDate, string shoppingItems)
+        {
+            Title = title;
+            ShoppingDate = shoppingDate;
+            ShoppingItems = shoppingItems;
+        }
+    }
+}
