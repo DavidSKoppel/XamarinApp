f62687abb2c1646d11458b856f03232919542787
DavidSKoppel <davi3442@edu.sde.dk>
2022-01-14 11:31:34 +0100

    New load list
    Still in the making

--- ToDoListXamarin/ToDoListXamarin/ViewModels/MainPageViewModel.cs
+++ ToDoListXamarin/ToDoListXamarin/ViewModels/MainPageViewModel.cs
@@ -3,26 +3,45 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
 using Xamarin.Forms;
+using ToDoListXamarin.Services;
+using ShoppingList.Services;
+using System.Threading.Tasks;
 
 namespace ToDoListXamarin
 {
     public class MainPageViewModel
     {
-        public ShoppingListAndItems shoppingList;
+        public IDataStore<ShoppingListAndItems> DataStore => DependencyService.Get<IDataStore<ShoppingListAndItems>>();
 
+        public ShoppingListAndItems selectedList;
+        public bool IsBusy { get; set; }
+
+        public ObservableCollection<ShoppingListAndItems> Lists { get;  }
+        public Command LoadListsCommand { get; }
         public Command<ShoppingListAndItems> ItemTapped { get; }
-        public ObservableCollection<ShoppingListAndItems> ShoppingLists { get; set; }
+        
         public MainPageViewModel()
         {
-            ItemTapped = new Command<ShoppingListAndItems>(OnItemSeleceted);
-            ShoppingLists = new ObservableCollection<ShoppingListAndItems>();
+            Lists = new ObservableCollection<ShoppingListAndItems>();
+            LoadListsCommand = new Command(async () => await LoadShoppingListsCommand());
 
-            ShoppingLists.Add(new ShoppingListAndItems(1, "Todo 1", Convert.ToDateTime("12-01-20")));
-            ShoppingLists.Add(new ShoppingListAndItems(2, "Todo 2", Convert.ToDateTime("12-01-20")));
-            ShoppingLists.Add(new ShoppingListAndItems(3, "Todo 3", Convert.ToDateTime("12-01-20")));
+            ItemTapped = new Command<ShoppingListAndItems>(OnItemSelected);
         }
 
-        async void OnItemSeleceted(ShoppingListAndItems obj)
+        async Task LoadShoppingListsCommand()
+        {
+            IsBusy = true;
+
+            Lists.Clear();
+            var lists = await DataStore.GetItemsAsync(true);
+            foreach (var list in lists)
+            {
+                Lists.Add(list);
+            }
+            IsBusy = false;
+        }
+
+        async void OnItemSelected(ShoppingListAndItems obj)
         {
             if (obj == null)
                 return;
