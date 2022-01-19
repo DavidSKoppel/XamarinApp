dcecf671d739196bd6d3f09a7c9b6999dd0bd398
DavidSKoppel <davi3442@edu.sde.dk>
2022-01-19 08:29:57 +0100

    Added create functionallity to createlistviewmodel

--- ToDoListXamarin/ToDoListXamarin/ViewModels/CreateListViewModel.cs
+++ ToDoListXamarin/ToDoListXamarin/ViewModels/CreateListViewModel.cs
@@ -6,21 +6,23 @@ using System.Windows.Input;
 using Xamarin.Forms;
 using ToDoListXamarin;
 using ToDoListXamarin.Models;
+using System.Threading.Tasks;
 
 namespace ToDoListXamarin.ViewModels
 {
-    public class ShoppingTodoListViewModel
+    public class CreateListViewModel : BaseViewModel
     {
         public ObservableCollection<ToDoItem> ToDoItems { get; set; }
-        public ShoppingTodoListViewModel()
+        public CreateListViewModel()
         {
             ToDoItems = new ObservableCollection<ToDoItem>();
-
             ToDoItems.Add(new ToDoItem(1, "Cheese", false, 1));
+
+            SaveCommand = new Command(SaveListCommandAsync);
         }
 
+        public Command SaveCommand { get; }
         public ICommand AddTodoCommand => new Command(AddTodoItem);
-        public ICommand SaveCommand => new Command(SaveListCommand);
         public string NewListTitle { get; set; }
         public DateTime SelectedListDate { get; set; }
         public string NewTodoInputValue { get; set; }
@@ -29,8 +31,24 @@ namespace ToDoListXamarin.ViewModels
             ToDoItems.Add(new ToDoItem(1, NewTodoInputValue, false, 1));
         }
 
-        void SaveListCommand()
+        public async void SaveListCommandAsync()
         {
+            List<ShoppingListAndItems> templist = new List<ShoppingListAndItems>(await DataStore.GetItemsAsync());
+            int newId = 0;
+            foreach(var numlist in templist) 
+            {
+                if (numlist.Id > newId)
+                    newId = numlist.Id;
+            }
+            ShoppingListAndItems list = new ShoppingListAndItems()
+            {
+                Id = newId + 1,
+                Title = NewListTitle,
+                ShoppingDate = SelectedListDate,
+            };
+            await DataStore.AddItemAsync(list);
+
+            await Shell.Current.GoToAsync("..");
             //ShoppingLists.Add(new ShoppingListAndItems(1, NewListTitle, SelectedListDate));
         }
     }
